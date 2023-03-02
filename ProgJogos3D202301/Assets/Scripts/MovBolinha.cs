3abb35c2e5c1aa101775a6b6389496d09a83ffff
Edson Martin Feitosa <edsonmfeitosa@hotmail.com>
2023-03-02 20:59:49 -0300

    pulo da bolinha

--- ProgJogos3D202301/Assets/Scripts/MovBolinha.cs
+++ ProgJogos3D202301/Assets/Scripts/MovBolinha.cs
@@ -7,12 +7,15 @@ public class MovBolinha : MonoBehaviour
     public float movX;
     public float movZ;
     public float velocidade;
+    public float forcaPulo;
     //teclas
     public KeyCode paraCima;
     public KeyCode paraBaixo;
     public KeyCode esquerda;
     public KeyCode direita;
     private Rigidbody fisica;
+
+    private bool estaNoChao = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,24 +30,24 @@ public class MovBolinha : MonoBehaviour
         movZ = Input.GetAxis("Vertical");
         #endregion
         #region 2ª maneira de pegar entrada do jogador
-        movX = 0;
-        movZ = 0;
-        if (Input.GetKey(paraCima))
-        {
-            movZ = 1;
-        }
-        if (Input.GetKey(paraBaixo))
-        {
-            movZ = -1;
-        }
-        if (Input.GetKey(esquerda))
-        {
-            movX = -1;
-        }
-        if (Input.GetKey(direita))
-        {
-            movX = 1;
-        }
+        //movX = 0;
+        //movZ = 0;
+        //if (Input.GetKey(paraCima))
+        //{
+        //    movZ = 1;
+        //}
+        //if (Input.GetKey(paraBaixo))
+        //{
+        //    movZ = -1;
+        //}
+        //if (Input.GetKey(esquerda))
+        //{
+        //    movX = -1;
+        //}
+        //if (Input.GetKey(direita))
+        //{
+        //    movX = 1;
+        //}
         #endregion
 
         Vector3 mov = new Vector3(movX * Time.deltaTime * velocidade,
@@ -61,5 +64,26 @@ public class MovBolinha : MonoBehaviour
         #region 3ª forma de movimentação
         //fisica.velocity = mov * velocidade;
         #endregion
+
+        //pulo da bolinha
+        if (Input.GetKeyDown(KeyCode.Space) && estaNoChao == true)
+        {
+            fisica.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "chao")
+        {
+            estaNoChao = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "chao")
+        {
+            estaNoChao = false;
+        }
     }
 }
